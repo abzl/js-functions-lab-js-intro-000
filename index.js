315688677b2bef1acb8d9401f5b946cbda6808c1
Alejandro Bazalar <pbzl.us@gmail.com>
2020-11-14 21:17:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
   return `Happy holidays, + ${name}!`
 }
 
-function happyCustomHolidayTo(happyHolidaysTo, name) {
+function happyCustomHolidayTo(holiday, name) {
   return `Happy + ${holiday}, + ${name}!`
 }
